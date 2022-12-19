17ef973785c79cf9f60601d7dbac57b0f2e7b384
陳廷祥 <xoxyzx@gmail.com>
2022-12-19 20:38:36 +0800

    Initial Commit

---
+++ project!/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  project!
+//
+//  Created by 許雯淇 on 2022/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project!/project_App.swift
@@ -0,0 +1,17 @@
+//
+//  project_App.swift
+//  project!
+//
+//  Created by 許雯淇 on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
